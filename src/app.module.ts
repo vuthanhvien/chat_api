0a5b692f6cac403c9f56e344e121a8c4a653c214
Vien.BIT <Vien.vu@bitelle.com>
2025-05-22 08:20:02 +0700

    12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ChatModule } from './chat/chat.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ChatModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
