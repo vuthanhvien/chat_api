1f68a17793699a8239faa1d51120e03b05914c88
Vien.BIT <Vien.vu@bitelle.com>
2025-05-26 22:28:49 +0700

    12

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // or specify allowed origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT ?? 4321);
 }
 bootstrap();
